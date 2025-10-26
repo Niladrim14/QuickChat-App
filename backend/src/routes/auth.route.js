a906152e7a400e954aa7d1fb1ee1aea9b2cc282d
Niladrim14 <niladrim004@gmail.com>
2025-10-27 01:24:44 +0530

    minor fix

--- backend/src/routes/auth.route.js
+++ backend/src/routes/auth.route.js
@@ -1,8 +1,9 @@
 import express from 'express';
 import { signup ,login , logout, updateProfile } from '../controllers/auth.controllers.js';
 import { protectRoute } from '../middleware/auth.middleware.js';
-
 import { arcjetProtection } from '../middleware/arcjet.middleware.js';
+
+const router = express.Router();
 
 router.post('/signup', arcjetProtection, signup);
 
